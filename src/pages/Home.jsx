6011ec8b716b7565863d005934abd06556444f54
Sahib Huseynov <96313603+sahibhuseynov@users.noreply.github.com>
2023-03-14 12:49:06 +0400

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,22 @@ const Home = () => {
     <div className='home'>
       <div className="home__title">
         <motion.h1
-        initial={{ x: -100}}
+        initial={{ x: -200}}
         animate={{ x: 0}}
         transition={{ duration: 0.8, type: 'spring', stiffness: 120 }}
         >Sahib Huseynov</motion.h1>
-        
-      </div>
-      
-<TypeAnimation
+        <TypeAnimation
     sequence={['Frontend Web Developer.', ]}
     speed={55} 
     wrapper="h3"
-
-    
-    
   />
+  <div className='home__btn__group'>
+    <button className='btn2'><span>View Work</span></button>
+    <button className='btn'>Contact Me</button>
+  </div>
+      </div>
+      
+
     </div>
   )
 }
