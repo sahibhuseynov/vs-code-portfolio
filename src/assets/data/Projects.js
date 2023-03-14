62d050bb09091f608eed5ba285ffcb6b96ea5a36
Sahib Huseynov <96313603+sahibhuseynov@users.noreply.github.com>
2023-03-14 23:20:13 +0400

    projects

---
+++ src/assets/data/Projects.js
@@ -0,0 +1,32 @@
+const Projects = [
+    {
+        id: 1,
+        name: 'WhiteMind',
+        description: 'This is an education academy website',
+        image: 'https://picsum.photos/200/300',
+        link: 'http://whitemindacademy.com/',
+        github: 'https://www.google.com',
+        tech: ['React', 'Node', 'Express', 'MongoDB', 'Sass']
+    },
+    {
+        id: 2,
+        name: 'Cihanshop',
+        description: 'This is an e-commerce website',
+        image: 'https://picsum.photos/200/300',
+        link: 'https://cihanshop.netlify.app/',
+        github: 'https://github.com/sahibhuseynov/Cihanshop',
+        tech: ['React','Redux-toolkit', 'Node', 'Express', 'MongoDB', 'Sass','Axios']
+    },
+    {
+        id: 3,
+        name: 'Trimon',
+        description: 'Website that organizes esports gaming tournaments',
+        image: 'https://picsum.photos/200/300',
+        link: 'https://trimon.netlify.app/',
+        github: ''
+        tech: ['React','Redux-toolkit', 'Node', 'Express', 'MongoDB', 'Sass','Axios']
+    }
+]
+
+
+export default Projects;
