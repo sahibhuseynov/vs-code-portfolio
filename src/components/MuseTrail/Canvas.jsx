e3cf7bce603c3cc480261af954001466a3496007
Sahib Huseynov <96313603+sahibhuseynov@users.noreply.github.com>
2023-03-16 20:01:32 +0400

    ss

--- src/components/MuseTrail/Canvas.jsx
+++ src/components/MuseTrail/Canvas.jsx
@@ -1,91 +1,100 @@
 import React, { useRef, useEffect } from "react";
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.lifetime = 0;
-  }
-}
-
 const Canvas = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    const points = [];
+    let mouseX = 0;
+    let mouseY = 0;
+    const particles = [];
 
-    const addPoint = (x, y) => {
-      const point = new Point(x, y);
-      points.push(point);
+    const addParticle = (x, y) => {
+      const particle = {
+        x,
+        y,
+        vx: Math.random() * 1 - 0.5,
+        vy: Math.random() * 1 - 0.5,
+        radius: Math.random() * 20 + 10,
+        opacity: 0.7,
+        life: Math.floor(Math.random() * 60) + 40
+      };
+      particles.push(particle);
     };
 
     const handleMouseMove = (event) => {
       const rect = canvas.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
-      addPoint(x, y);
+      mouseX = event.clientX - rect.left;
+      mouseY = event.clientY - rect.top;
+      for (let i = 0; i < 3; i++) {
+        addParticle(mouseX, mouseY);
+      }
     };
 
     document.addEventListener("mousemove", handleMouseMove);
 
-    const animatePoints = () => {
+    const animateParticles = () => {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      const duration = (0.7 * (1 * 1000)) / 60; // Last 80% of a frame per point
 
-      for (let i = 0; i < points.length; ++i) {
-        const point = points[i];
-        let lastPoint;
+      for (let i = 0; i < particles.length; i++) {
+        const particle = particles[i];
+        particle.x += particle.vx;
+        particle.y += particle.vy;
 
-        if (points[i - 1] !== undefined) {
-          lastPoint = points[i - 1];
-        } else lastPoint = point;
-
-        point.lifetime += 1;
-
-        if (point.lifetime > duration) {
-          // If the point dies, remove it.
-          points.shift();
-        } else {
-          // Otherwise animate it:
-          // As the lifetime goes on, lifePercent goes from 0 to 1.
-          const lifePercent = point.lifetime / duration;
-          const spreadRate = 7 * (1 - lifePercent);
-
-          ctx.lineJoin = "round";
-          ctx.lineWidth = spreadRate;
-
-          // As time increases decrease r and b, increase g to go from purple to green.
-          const red = Math.floor(190 - 190 * lifePercent);
-          const green = 0;
-          const blue = Math.floor(210 + 210 * lifePercent);
-          ctx.strokeStyle = `rgb(${red},${green},${blue}`;
-
-          ctx.beginPath();
-          ctx.moveTo(lastPoint.x, lastPoint.y);
-          ctx.lineTo(point.x, point.y);
-          ctx.stroke();
-          ctx.closePath();
+        // apply some resistance to slow down particle velocity
+        particle.vx *= 0.99;
+        particle.vy *= 0.99;
+    
+        particle.opacity = particle.life / 100;
+        particle.life--;
+    
+        if (particle.life <= 0) {
+          particles.splice(i, 1);
+          i--;
+          continue;
         }
+    
+        ctx.beginPath();
+        const gradient = ctx.createRadialGradient(
+          particle.x,
+          particle.y,
+          0,
+          particle.x,
+          particle.y,
+          particle.radius
+        );
+        gradient.addColorStop(0, `rgba(0, 0, 255, ${particle.opacity})`);
+        gradient.addColorStop(1, `rgba(0, 0, 255, 0)`);
+        ctx.fillStyle = gradient;
+        ctx.arc(
+          particle.x,
+          particle.y,
+          particle.radius,
+          0,
+          2 * Math.PI,
+          false
+        );
+        ctx.fill();
       }
-      requestAnimationFrame(animatePoints);
+    
+      requestAnimationFrame(animateParticles);
     };
-
-    animatePoints();
-
+    
+    animateParticles();
+    
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={window.innerWidth}
-      height={window.innerHeight}
-    />
+  <canvas
+     ref={canvasRef}
+     width={window.innerWidth}
+     height={window.innerHeight}
+   />
   );
-};
-
-export default Canvas;
+  };
+  
+  export default Canvas;    
